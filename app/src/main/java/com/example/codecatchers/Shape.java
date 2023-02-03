4bdd2fd667123bd57b9e32794cc5f070e751f4d8
Jcatz13 <58762789+JcatZ13@users.noreply.github.com>
2023-02-03 15:38:43 -0700

    Update Shape.java

--- app/src/main/java/com/example/codecatchers/Shape.java
+++ app/src/main/java/com/example/codecatchers/Shape.java
@@ -5,10 +5,7 @@ public abstract class Shape {
     public int x;
     public int y;
     String color = "$PUT_YOUR_CHOICE_OF_COLOR";
-<<<<<<< HEAD
-    String color = "blue";
-=======
-    
+
     String color = 'green';
->>>>>>> 19a8a305c93c44e47c54400e875bf289ecc6e6df
+
 }
